2c52a54317abb18c66d272023f9ccd4c9ea74d1d
samie820 <samuelomole70@gmail.com>
2017-10-25 23:11:22 +0100

    Changed the requirements.txt

--- django_ig/settings.py
+++ django_ig/settings.py
@@ -64,19 +64,19 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'django_ig.urls'
 
-redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
 
 
-CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
-        'OPTIONS': {
-            'DB': 0,
-            'PASSWORD': redis_url.password,
-        }
-    }
-}
+
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'redis_cache.RedisCache',
+#         'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+#         'OPTIONS': {
+#             'DB': 0,
+#             'PASSWORD': redis_url.password,
+#         }
+#     }
+# }
 
 CHANNEL_LAYERS = {
     "default": {
